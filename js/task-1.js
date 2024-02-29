2249e3fbf15a1e1003f43ba2580a9bbacdedd38e
AnaPuris <apuris911@gmail.com>
2024-02-29 17:05:28 +0000

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,31 @@
 'use strict';
 
 
-function makeTransaction(quantity, pricePerDroid) {
-    return (`You ordered ${quantity} droids worth ${pricePerDroid * quantity} credits!`);
-}
+// function makeTransaction(quantity, pricePerDroid) {
+//     return (`You ordered ${quantity} droids worth ${pricePerDroid * quantity} credits!`);
+// }
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+// console.log(makeTransaction(5, 3000));
+// console.log(makeTransaction(3, 1000));
+// console.log(makeTransaction(10, 500));
+
+// let msg;
+// const isGreater = 1 > 0;
+
+// console.log("Before");
+// if (isGreater) {
+//     msg = `Inside if statement when truthy value`;
+// } else {
+//     msg = `Inside if statement, when gets falsy value`;
+// }
+
+// console.log("After");
+// console.log(msg);
+
+console.log("Before");
+
+for (let index = 0; index <= 10; index += 1){
+    console.log(`Inside cycle`);
+}
+    
+console.log("After");
