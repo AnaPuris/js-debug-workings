80ba6aa88e595d607f5fb4bbea41a764a41a6f0a
AnaPuris <apuris911@gmail.com>
2024-03-26 10:25:07 +0000

    upd

--- js/tasks-from-webinars.js
+++ js/tasks-from-webinars.js
@@ -30,12 +30,29 @@
 
 
 
-function getExtremeElements(array) {
-let lastElement = array.length - 1;
-let arrayNew = [array[0], array[lastElement]];
-return arrayNew;
+// function getExtremeElements(array) {
+// let lastElement = array.length - 1;
+// let arrayNew = [array[0], array[lastElement]];
+// return arrayNew;
+// }
+
+// console.log(getExtremeElements([1, 2, 3, 4, 5])); //[1, 5]
+// console.log(getExtremeElements(["Earth", "Mars", "Venus"])); //["Earth", "Venus"]
+// console.log(getExtremeElements(["apple", "peach", "pear", "banana"])); //["apple", "banana"]
+
+
+const employees = [
+    { name: "Jessica", hours: 30 },
+    { name: "Sarah", hours: 20 },
+    { name: "David", hours: 45 },
+    { name: "John", hours: 40 },
+    { name: "Susan", hours: 38},
+]
+
+function calculateWeeklyPay(employees) {
+    let totalPay = 0;
+    for (let i = 0; i <= employees.length; i++) {
+        totalPay = hours * rate;
+    } return totalPay;
 }
 
-console.log(getExtremeElements([1, 2, 3, 4, 5])); //[1, 5]
-console.log(getExtremeElements(["Earth", "Mars", "Venus"])); //["Earth", "Venus"]
-console.log(getExtremeElements(["apple", "peach", "pear", "banana"])); //["apple", "banana"]
